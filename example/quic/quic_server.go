8da818911b058ce40b6723e76c584e9e39edebe1
helloh2o <aiyongay@126.com>
2020-12-24 10:21:17 +0800

    add Quic(http3) example

---
+++ example/quic/quic_server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"io/ioutil"
+	"lucky/core/inet"
+	"lucky/example/comm/msg"
+	"math/big"
+	"os"
+)
+
+func main() {
+	msg.SetEncrypt(msg.Processor)
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	err = ioutil.WriteFile("k.key", keyPEM, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile("c.cert", certPEM, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+	pem, err := tls.LoadX509KeyPair("./c.cert", "./k.key")
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{pem},
+		NextProtos:   []string{"quic-hello-example"},
+	}
+	if s, err := inet.NewQUICServer("localhost:2024", msg.Processor, tlsConfig); err != nil {
+		panic(err)
+	} else {
+		err = s.Run()
+	}
+}
