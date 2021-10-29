b1e9862bbc2e56667db5446620945f6d701c475d
helloh2o <aiyongay@126.com>
2021-10-29 17:05:12 +0800

    SyncStrWithTimeout

--- utils/sync.go
+++ utils/sync.go
@@ -22,3 +22,19 @@ func SyncObjByStr(objKey string) func() {
 		<-objChan
 	}
 }
+
+// SyncStrWithTimeout 锁定一个字符串的同步操作,允许过期
+func SyncStrWithTimeout(objKey string, duration time.Duration) func() {
+	_, ok := syncObjChan.Get(objKey)
+	if !ok {
+		syncObjChan.Set(objKey, make(chan struct{}, 1), duration)
+	}
+	chObj, _ := syncObjChan.Get(objKey)
+	// lock the objKey
+	objChan := chObj.(chan struct{})
+	objChan <- struct{}{}
+	return func() {
+		// release the objChan
+		<-objChan
+	}
+}
