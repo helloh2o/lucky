8a2421f0f63af5927993402462e9bf397a354ab1
helloh2o <8416471+helloh2o@user.noreply.gitee.com>
2024-01-23 14:36:46 +0800

    Add notify callback

--- utils/distributed_sync.go
+++ utils/distributed_sync.go
@@ -10,8 +10,10 @@ import (
 
 // 等待者
 type Waiter struct {
+	Id string
 	sync.Mutex
 	channels map[string]chan struct{}
+	OnNotify func(string)
 	wk       map[string]int
 }
 
@@ -28,14 +30,24 @@ Try:
 	return done
 }
 
-// RDLockOpCancel redis 等待分布式锁，直到超时取消获取锁
-func RDLockOpCancel(operation string, timeout time.Duration) (func(), bool) {
-	tk := time.NewTicker(timeout)
+// RDLockOpWaitTimeout 自定义超时操作
+func RDLockOpWaitTimeout(operation string, timeout time.Duration) func() {
 Try:
-	done, ok, wait := do(operation, time.Hour*6)
+	done, ok, wait := do(operation, timeout)
+	if !ok {
+		<-wait
+		goto Try
+	}
+	return done
+}
+
+// RDLockOpWithContext redis 上下文获取锁
+func RDLockOpWithContext(ctx context.Context, operation string) (func(), bool) {
+Try:
+	done, ok, wait := do(operation, time.Hour)
 	if !ok {
 		select {
-		case <-tk.C:
+		case <-ctx.Done():
 			return func() {}, true
 		case <-wait:
 			goto Try
@@ -51,7 +63,7 @@ func RDLockOp(operation string) (release func(), got bool, wait chan struct{}) {
 	return do(operation, defaultExpired)
 }
 
-// RDLockOp redis 分布式锁， 时间不够可能引发并发同步问题 release释放，got 是否获取锁,wait在线等待，直到获取锁
+// RDLockOpTimeout redis 分布式锁， 时间不够可能引发并发同步问题 release释放，got 是否获取锁,wait在线等待，直到获取锁
 func RDLockOpTimeout(operation string, timeout time.Duration) (release func(), got bool, wait chan struct{}) {
 	return do(operation, timeout)
 }
@@ -73,6 +85,9 @@ func do(key string, expired time.Duration) (func(), bool, chan struct{}) {
 			defer waiter.Unlock()
 			select {
 			case waiter.channels[key] <- struct{}{}:
+				if waiter.OnNotify != nil {
+					waiter.OnNotify(key)
+				}
 			default:
 			}
 		})
@@ -80,6 +95,10 @@ func do(key string, expired time.Duration) (func(), bool, chan struct{}) {
 		release := func() {
 			waiter.Lock()
 			defer waiter.Unlock()
+			// 释放通知
+			if waiter.OnNotify != nil {
+				waiter.OnNotify(key)
+			}
 			// waiter channel is existed
 			if _, existed := waiter.channels[key]; !existed {
 				return
@@ -96,7 +115,7 @@ func do(key string, expired time.Duration) (func(), bool, chan struct{}) {
 				if waiter.wk[key] == 0 {
 					delete(waiter.channels, key)
 					delete(waiter.wk, key)
-					log.Release("===> key:%s, all sync lock request wait done. <===", key)
+					log.Debug("===> key:%s, all sync lock request wait done. <===", key)
 				}
 			default:
 			}
@@ -117,10 +136,13 @@ func do(key string, expired time.Duration) (func(), bool, chan struct{}) {
 			left := cache.RedisC.TTL(context.Background(), key).Val()
 			log.Release("key::%s, ttl:%d s", key, left/time.Second)
 			time.AfterFunc(left, func() {
-				log.Release("redis key:%s expired.", key)
+				log.Debug("redis key:%s expired.", key)
 				// 时间到，写入等待着
 				select {
 				case wc <- struct{}{}:
+					if waiter.OnNotify != nil {
+						waiter.OnNotify(key)
+					}
 				default:
 				}
 			})
