4fb700663a1d01e59a4f80c43ff3520ce81a5df6
helloh2o <8416471+helloh2o@user.noreply.gitee.com>
2024-03-08 09:31:24 +0800

    set main db

--- xdb/qpsdb.go
+++ xdb/qpsdb.go
@@ -9,7 +9,17 @@ import (
 
 var (
 	tokenLimiter *utils.RateLimiter
+	mainDB       *gorm.DB
 )
+
+// SetMainDB 设置主DB
+func SetMainDB(main *gorm.DB, DSN string) {
+	if main == nil {
+		panic("mainDB is nil")
+	}
+	log.Release("mainDB is set, DSN :%s", DSN)
+	mainDB = main
+}
 
 // InitQpsDB 初始化同步等待QPS DB
 func InitQpsDB(limit int, interval time.Duration) {
@@ -21,6 +31,9 @@ func InitQpsDB(limit int, interval time.Duration) {
 
 // QpsDB 获取DB对象，若超时则返回nil
 func QpsDB() *gorm.DB {
+	if mainDB == nil {
+		panic("mainDB is nil")
+	}
 	tokenLimiter.Wait()
-	return DB()
+	return mainDB
 }
